966c59601b778d8df241f0d7337024221e6d7db3
Evan Sherwood <evan@sherwood.io>
2017-12-12 15:04:55 -0800

    feat: added a div

--- modules/index.js
+++ modules/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default class A11yTable extends React.Component {
   render () {
-    return <h1>Hello world!</h1>
+    return (
+      <div>
+        <h1>Hello world!</h1>
+      </div>
+    )
   }
 }
