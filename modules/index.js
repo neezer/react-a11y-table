b44ed6d2fd5973a23c160ae97e89695ed0705f5d
Evan Sherwood <evan@sherwood.io>
2017-12-13 21:12:19 -0800

    feat: add FixedHeader dummy content

--- modules/index.js
+++ modules/index.js
@@ -1,8 +1,20 @@
 import React from 'react'
-// import FixedHeaderContainer from './FixedHeaderContainer'
+import FixedHeaderContainer from './FixedHeaderContainer'
+
+class FixedContent extends React.Component {
+  render () {
+    const { fixedRef } = this.props
+
+    return <h1 ref={fixedRef}>{`I'm a fixed header! w00t!`}</h1>
+  }
+}
+
+const Content = () => <h1>Table goes here!</h1>
 
 export default class A11yTable extends React.Component {
   render () {
-    return <h1>Hello world!</h1>
+    return (
+      <FixedHeaderContainer Content={Content} FixedContent={FixedContent} />
+    )
   }
 }
